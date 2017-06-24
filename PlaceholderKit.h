b503eda08a58ca8f9c1306c1e82e4acdf2a424c0
Todd Olsen <todd.olsen@me.com>
2017-06-24 12:29:11 -0400

    Added common header file.

---
+++ PlaceholderKit.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for PlaceholderKit.
+FOUNDATION_EXPORT double PlaceholderKitVersionNumber;
+
+//! Project version string for PlaceholderKit.
+FOUNDATION_EXPORT const unsigned char PlaceholderKitVersionString[];
+
+
